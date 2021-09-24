350b1c545bec6b76d40a11f53894759a4a5b1e97
Caitlin Wall <caitlinwall96@gmail.com>
2021-09-23 20:10:38 -0400

    Add functionality to links and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,27 @@
+//start quiz button
 var startButton = document.getElementById("start-button");
+//sections within the index page
 var mainPage = document.getElementById("main-page");
 var quiz = document.getElementById("quiz");
 var results = document.getElementById("results");
-var highscores = document.getElementById("highscores");
+var highScoresPage = document.getElementById("highscores");
+//question and answers
 var question = document.getElementById("question");
 var button0 = document.getElementById("choice0");
 var button1 = document.getElementById("choice1");
 var button2 = document.getElementById("choice2");
 var button3 = document.getElementById("choice3");
+//response to answer
 var correct = document.getElementById("correct");
 var wrong = document.getElementById("wrong");
+//Use for index to cycle through questions
 var counter = 0;
+//timer set-up
 var timeEl = document.querySelector("#timer");
 var secondsLeft = 76;
 var timerInterval;
+//Submit initials
+var submitInitials = document.getElementById("submit-button");
 
 var quizQuestions = [
 	{
@@ -69,25 +77,26 @@ var quizQuestions = [
 ];
 
 
-
 function startClick () {
+    //set timer to start running
     setTime();
     //Hide main page and show quiz
     mainPage.className = "hidden";
     quiz.className = "";
 }
-    
+ 
+//on click of start quiz button, run the startClick function
 startButton.addEventListener("click", startClick);
 
-//Start timer, and set an if statement to end the game if the timer runs out
+//Set-up timer to count down by one, and set an if statement to end the game if the timer runs out
 function setTime() {
-    // Sets interval in variable
+
     timerInterval = setInterval(function(){
     secondsLeft--;
     timeEl.textContent = "Time: " + secondsLeft;
 
     if(secondsLeft === 0) {
-        //Call function to score quiz
+        //Call function to receive score
         scoreQuiz();
         }
 
@@ -95,17 +104,22 @@ function setTime() {
 }
 
 function generateQuestions() {
+    //put questions in the question h2 and choices in the buttons
+    //counter will base it off of the index of the question
     question.textContent = quizQuestions[counter].question;
     button0.textContent = quizQuestions[counter].answers.a;
     button1.textContent = quizQuestions[counter].answers.b;
     button2.textContent = quizQuestions[counter].answers.c;
     button3.textContent = quizQuestions[counter].answers.d;
-
+    
+    //on click of any choice button, run nextQuestion function
     button0.onclick = nextQuestion;
     button1.onclick = nextQuestion;
     button2.onclick = nextQuestion;
     button3.onclick = nextQuestion;
 
+    //set the value of the button to be the answer for that button
+    //used later to compare value of button to correct answer 
     button0.setAttribute("value", quizQuestions[counter].answers.a);
     button1.setAttribute("value", quizQuestions[counter].answers.b);
     button2.setAttribute("value", quizQuestions[counter].answers.c);
@@ -113,38 +127,37 @@ function generateQuestions() {
 
 }
 
+//call function to generate the questions
 generateQuestions();
 
-var quizButtons = document.getElementsByClassName("quizButtons");
-
-// for (i=0; i< quizButtons.length; i++) {
-//     var selectedAnswer = quizButtons[i].addEventListener("click", nextQuestion);
-// }
-
-//If correct, show correct response, and move to next screen.
-//If wrong, subtract ten seconds from clock, show wrong response, and move to next screen. 
+//If correct, show 'correct' response, and move to next question.
+//If wrong, subtract ten seconds from clock, show 'wrong' response, and move to next question. 
 function nextQuestion() {
+
     //if the value matches the correct answer choice for this question...
-    if ( this.value === quizQuestions[counter].correctAnswer) {
+    if (this.value === quizQuestions[counter].correctAnswer) {
         //have correct show up for one second
         correct.className = "";
         setTimeout(function(){
-            //hide it again
+            //hide it again after second ends
             correct.className = "hidden"
         }, 1000)
+
     } else {
         //have wrong show up for one second
         wrong.className = "";
         setTimeout(function(){
-            //hide it again
+            //hide it again after second ends
             wrong.className = "hidden"
         }, 1000)
         //take 10 seconds off the clock
         secondsLeft -= 10;
     }
+
     //increment by one no matter what, to cycle through the questions
     counter++;
-    //if out of questions, scorequiz after a timeout
+
+    //if out of questions, scorequiz will run after a one-second timeout
     if (counter === quizQuestions.length) {
         //timeout allows a second for the last question to show a response before showing final score
         setTimeout(scoreQuiz, 1000);
@@ -152,21 +165,98 @@ function nextQuestion() {
         //generate a new question
         generateQuestions();
     }
-}
-    
+}   
 
 function scoreQuiz() {
     //score is the time left on the clock
     var score = secondsLeft;
     var finalScore = document.getElementById("finalScore");
     finalScore.innerHTML = "Your final score is " + score + "."
+
     // Stops execution of action, hides the quiz and shows the results
     clearInterval(timerInterval);
     quiz.className = "hidden";
     results.className = "";
 }
 
+//on click of the submit button, show high scores
+submitInitials.onclick = showScores;
 
+function showScores(event){
+    event.preventDefault();
+
+    //Hide results and show highscores
+    results.className = "hidden";
+    highScoresPage.className = "";
+
+    //set up a blank array to hold the scores
+    var highScores = [];
+
+    //Pull user input to an initials variable
+    var initials = document.getElementById("initials");
+    var score = secondsLeft;
+    //Put the initials and score into a newScore object
+    var newScore = {
+        initial: initials,
+        score: score
+    }
+    //Push the newScore into highScores
+    highScores.push(newScore)
+
+    //Put high scores into local storage
+    localStorage.setItem("highScores", JSON.stringify(highScores))
+
+    //Print scores to the page in order from highest score to lowest
+    //Pull scores from storage
+    var scores = JSON.parse(localStorage.getItem("highScores"));
+    //Sort scores to be high to low
+    scores.sort(function(a, b){return a-b});
+    //Add scores to li on page
+    var ol = document.querySelector(ol);
+    var li = document.createElement(li);
+    for (i=0; i <= 10; i++) {
+        li.textContent = score[i];
+        document.ol.appendChild(li);
+    }
+
+}
+
+//Go back button
+var goBack = document.getElementById("go-back");
+goBack.onclick = replay;
+
+function replay() {
+    //hide highscores and show main page
+    highScoresPage.className = "hidden";
+    mainPage.className = "";
+    //reset count, timer, and be ready for the click on the start button again
+    startButton.addEventListener("click", startClick);
+    counter = 0;
+    secondsLeft = 76;
+}
+
+//View HighScores from any part of the quiz
+var highScoreLink = document.getElementById("viewScores");
+highScoreLink.onclick = showScores;
+
+function showScores(){
+    //show highscores
+    highScoresPage.className = "";
+    //clear everything else
+    mainPage.className = "hidden";
+    quiz.className = "hidden";
+    results.className = "hidden";
+    //stop timer
+    clearInterval(timerInterval);
+}
+
+//Clear HighScores Button
+var clearBtn = document.getElementById("clear");
+clearBtn.onclick = clearScores;
+
+function clearScores() {
+    highScores = [];
+}
 
 
 
